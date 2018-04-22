fc39167a5a1559e87eb8303a6c8b739f2215803a
Brendanatalia <38529594+Brendanatalia@users.noreply.github.com>
2018-04-23 01:31:54 +0700

    Fungsi doSearch
    untuk melakukan pencarian

--- Find-Words.cpp
+++ Find-Words.cpp
@@ -5,6 +5,32 @@
 #include <iostream>
 using namespace std;
 
+ char puzzle[15][15] ={{'T','G','B','W','W','I','N','T','E','R','W','S','E','S','N'},
+	                {'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'},
+	                {'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'},
+	                {'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'},
+	                {'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'},
+	                {'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'},
+	                {'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'},
+	                {'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'},
+	                {'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'},
+	                {'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'},
+	                {'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'},
+	                {'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'},
+	                {'M','O','S','G','Z','C','Z','D','T','D','B','O','O','T','O'},
+                        {'P','D','C','R','Z','M','S','N','G','R','D','N','R','P','Z'},
+	                {'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'}};
+
+int callPuzzle(){
+	//pemanggil puzzle//					 
+for (int pb=0;pb<15;pb++){
+	for (int pk=0;pk<15;pk++){
+		cout<<puzzle[pb][pk]<<" ";
+	}cout<<endl;
+}
+}
+
+
 //--Agar semua kata menjadi huruf kapital
 char toUpper( char *word){
 	int x = 'A' - 'a';
@@ -28,37 +54,8 @@ int longOf(char *word){
 	return x;
 }
 
-int main(){
-  
-  char puzzle[15][15] ={{'T','G','B','W','W','I','N','T','E','R','W','S','E','S','N'},
-	                {'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'},
-	                {'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'},
-	                {'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'},
-	                {'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'},
-	                {'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'},
-	                {'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'},
-	                {'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'},
-	                {'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'},
-	                {'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'},
-	                {'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'},
-	                {'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'},
-	                {'M','O','S','G','Z','C','Z','D','T','D','B','O','O','T','O'},
-                        {'P','D','C','R','Z','M','S','N','G','R','D','N','R','P','Z'},
-	                {'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'}};
-
-//pemanggil puzzle//					 
-for (int pb=0;pb<15;pb++){
-	for (int pk=0;pk<15;pk++){
-		cout<<puzzle[pb][pk]<<" ";
-	}cout<<endl;
-}
-
-//input kata yang akan dicari
-char word[15];
-cin>>word;
-toUpper(word);
-longOf(word);
-
+int doSearch(char word[]){
+	
 int banyak = 0;
 	for (int pb=0;pb<15;pb++){
 	for (int pk=0;pk<15;pk++){
@@ -92,5 +89,17 @@ int banyak = 0;
 if (banyak > 0){
 	cout<<"ada";
 }
-else cout<<"tidak ada";	
+else cout<<"tidak ada";	}
+
+int main(){
+
+callPuzzle();
+
+//input kata yang akan dicari
+char word[15];
+cin>>word;
+toUpper(word);
+longOf(word);
+doSearch(word);
+
 }
