70caf5403c3b28342bde9cf6f7dfac73548ce56a
NaomyTiaraDewi <38529620+NaomyTiaraDewi@users.noreply.github.com>
2018-04-22 23:36:59 +0700

    toUpper Function

--- Find-Words.cpp
+++ Find-Words.cpp
@@ -5,23 +5,37 @@
 #include <iostream>
 using namespace std;
 
+//--Agar semua kata menjadi huruf kapital
+char toUpper( char *word){
+	int x = 'A' - 'a';
+	if (x < 0) x*=-1;
+	
+	for (int i=0;word[i];i++){
+		if ('A' < 'a'){
+		if (word[i] >= 'a') word[i]-=x;}
+		else{
+		if (word[i] <= 'A') word[i]+=x;}
+		
+	}
+}
+
 int main(){
   
   char puzzle[15][15] ={{'T','G','B','W','W','I','N','T','E','R','W','S','E','S','N'},
-	                      {'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'},
-	                      {'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'},
-	                      {'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'},
-	                      {'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'},
-	                      {'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'},
-	                      {'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'},
-	                      {'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'},
-	                      {'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'},
-	                      {'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'},
-	                      {'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'},
-	                      {'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'},
-	                      {'M','O','S','G','Z','C','Z','D','T','D','B','O','O','T','O'},
+	                {'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'},
+	                {'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'},
+	                {'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'},
+	                {'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'},
+	                {'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'},
+	                {'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'},
+	                {'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'},
+	                {'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'},
+	                {'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'},
+	                {'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'},
+	                {'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'},
+	                {'M','O','S','G','Z','C','Z','D','T','D','B','O','O','T','O'},
                         {'P','D','C','R','Z','M','S','N','G','R','D','N','R','P','Z'},
-	                      {'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'}};
+	                {'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'}};
 
 //pemanggil puzzle//					 
 for (int pb=0;pb<15;pb++){
@@ -29,4 +43,12 @@ for (int pb=0;pb<15;pb++){
 		cout<<puzzle[pb][pk]<<" ";
 	}cout<<endl;
 }
+
+//input kata yang akan dicari
+char word[15];
+cin>>word;
+toUpper(word);
+
+
+	
 }
